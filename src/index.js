810445482ccfdcf2bf3340a6cd0c76e88397fab4
Alex <elmejol404@gmail.com>
2025-04-17 10:39:18 -0400

    add advanced math

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-function sum(a, b) {
-    return a + b;
+const { add, subtract, multiply, divide } = require('../utils/math');
+
+function calculator(a, b, operation) {
+  switch (operation) {
+    case 'add':
+      return add(a, b);
+    case 'subtract':
+      return subtract(a, b);
+    case 'multiply':
+      return multiply(a, b);
+    case 'divide':
+      if (b === 0) throw new Error('Cannot divide by zero');
+      return divide(a, b);
+    default:
+      throw new Error('Invalid operation');
   }
-  
-  module.exports = { sum };
-  
+}
+
+module.exports = { calculator };
